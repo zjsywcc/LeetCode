21cb8c47298285a7c74d36dae4793e72074fc1d8
zjsywcc <zjsywcc@163.com>
2016-09-29 21:21:04 +0800

    add 72

--- DynamicProgramingA/72/src/Main.java
+++ DynamicProgramingA/72/src/Main.java
@@ -1,10 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String str1 = "sea";
+        String str2 = "eat";
+        System.out.println(minDistance(str1, str2));
     }
 
     public static int minDistance(String word1, String word2) {
-        
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 2];
+        for(int i = 0; i < len1 + 1; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 0; j < len2 + 1; j++) {
+            dp[0][j] = j;
+        }
+        for(int i = 1; i < len1 + 1; i++) {
+            for(int j = 1; j < len2 + 1; j++) {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], 1 + Math.min(dp[i - 1][j], dp[i][j - 1]));
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, 1 + Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 }
